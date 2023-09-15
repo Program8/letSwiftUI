35a562f771b75966a1156d6dc5b8399da72de4bb
Vijay Sachan <appsgem123@gmail.com>
2023-09-15 22:47:59 +0530

    Initial Commit

---
+++ letSwiftUI/letSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  letSwiftUIApp.swift
+//  letSwiftUI
+//
+//  Created by Vijay Sachan on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct letSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
